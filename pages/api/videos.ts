4d215ab7373142d26e2839b8aaa4bc2eb3c90a1e
ADMohamedd <94984384+Janku13@users.noreply.github.com>
2022-07-05 13:59:49 -0300

    adding upload option

--- pages/api/videos.ts
+++ pages/api/videos.ts
@@ -1,13 +1,48 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import busboy from 'busboy'
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs'
 
-type Data = {
-  name: string
+export const config = {
+  api: {
+    bodyParser:false,
+  }
 }
+
+const fileId = uuidv4()
+
+function uploadVideoStream(req:NextApiRequest, res:NextApiResponse) {
+  const bb = busboy({ headers: req.headers })
+    setTimeout(() => { }, 10000)
+  bb.on('file', (_, file, info) => {
+    console.log(file)
+    const fileName = `${fileId}${info.filename}`
+    const filePath = `./videos/${fileName}`
+    const stream = fs.createWriteStream(filePath)
+    file.pipe(stream)
+    
+  })
+
+  bb.on('close', () => {
+    res.writeHead(200, { Connection: "close" });
+    res.end('Finished uploading')
+  })
+  req.pipe(bb)
+  return
+}
+
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'Mohamed' })
+  const method = req.method
+  if (method === 'GET') {
+    return res.status(200).json({ name: 'Mohamed' })
+  }
+  if (method === 'POST') {
+    return uploadVideoStream(req,res)
+  }
+  return res.status(405).json({error:`Method ${method} in not allowed`})
 }
